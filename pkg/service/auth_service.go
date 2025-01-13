87d31dab308211d85c2241e2744b4ccafaed6e0a
MythosMa <mythosmayuan@gmail.com>
2025-01-13 20:20:13 +0800

    add register user

--- pkg/service/auth_service.go
+++ pkg/service/auth_service.go
@@ -9,10 +9,10 @@ import (
 	"go_web_server/db"
 )
 
-func RegisterUser(username, password, email string) string {
+func RegisterUser(username, password, email string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return "服务器错误"
+		return "服务器错误", err
 	}
 
 	_, err = db.DB.Exec("INSERT INTO users (username, password, avatar) VALUES (?, ?, ?)", username, string(hashedPassword), email)
@@ -20,11 +20,11 @@ func RegisterUser(username, password, email string) string {
 	if err != nil {
 		log.Println(err)
 		if strings.Contains(err.Error(), "Duplicate entry") {
-			return "用户已存在"
+			return "用户已存在", err
 		} else {
-			return "服务器错误"
+			return "服务器错误", err
 		}
 	}
 
-	return "用户注册成功"
+	return "用户注册成功", nil
 }
